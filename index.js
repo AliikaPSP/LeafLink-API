88288148e1eec4caea7b62c266eca333624622e1
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2024-11-19 12:35:16 +0200

    Project setup 10. The Project Backend has a /docs endpoint to test the API with

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const app = require('express')();
 const port = 8080;
-app.listen(port, () => {console.log(`SApi on saadaval aadressil: http://localhost:${port} `);});    
+const app = require('express')();
+//npm install swagger-ui-express 
+const swaggerUI = require
+('swagger-ui-express');
+const swaggerDoc = require(".docs/swagger.json");
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+
+app.listen(port, () => {console.log(`Api on saadaval aadressil: http://localhost:${port} `);});    
