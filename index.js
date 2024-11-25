311954cb6e0ecc8c2ad5c34fa9d4034644aadf3a
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2024-11-25 16:32:13 +0200

    As an app developer, I want to be able to ADD new plant in my app

--- index.js
+++ index.js
@@ -5,25 +5,29 @@ const swaggerUI = require
 ('swagger-ui-express');
 const yamljs = require('yamljs');
 const swaggerDoc = yamljs.load('./docs/swagger.yaml');
+var express = require('express');
 
 const plants = [
 
     {
-        PlantID: 1, PlantName: "Rose", 
+        PlantID: 1, 
+        PlantName: "Rose", 
         Description: "This is a beautiful rose plant.", 
         Size: "Medium", 
         PlantRequirements: "Water, Soil, Light", 
         PlantInstructions:"Water 5-6 times a week, Light: indirect"
     },
     {
-        PlantID: 2, PlantName: "Lily", 
+        PlantID: 2, 
+        PlantName: "Lily", 
         Description: "This is a beautiful lily plant.", 
         Size: "Large",  
         PlantRequirements: "Water, Light", 
         PlantInstructions:"A body of water. Light: indirect"
     },
     {
-        PlantID: 3, PlantName: "Tulips", 
+        PlantID: 3, 
+        PlantName: "Tulips", 
         Description: "This is a beautiful tulip plant.", 
         Size: "Small",  
         PlantRequirements: "Water, Soil, Light", 
@@ -33,6 +37,7 @@ const plants = [
 ]
 
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+app.use(express.json()); //for parsing application/json
 
 app.get('/plants', (req, res) => {res.send(plants)});   
 app.get('/plants/:id', (req, res) => {
@@ -46,5 +51,26 @@ app.get('/plants/:id', (req, res) => {
     res.send(plants[req.params.id-1])
 })
 
+app.post('/plants', (req, res) => {
+    if (!req.body.PlantName || !req.body.Description || !req.body.Size || !req.body.PlantRequirements ||!req.body.PlantInstructions) {
+        return res.send(400).send({error: "One or multiple parameters are missing"});
+    }
+    
+    let plant = {
+        PlantID: plants.length + 1,
+        PlantName: req.body.PlantName,
+        Description: req.body.Description,
+        Size: req.body.Size,
+        PlantRequirements: req.body.PlantRequirements,
+        PlantInstructions: req.body.PlantInstructions
+    }
+    plants.push(plant);
+    res.status(201).location('${getBaseURL(req)}/plants/${plants.length}').send(plant);
+})
 app.listen(port, () => {console.log(`Api on saadaval aadressil: http://localhost:${port} `);});    
 
+function getBaseURL(req) 
+{
+    return req.connection && req.connection.encrypted ?
+    "https" : "http" + `://${req.headers.host}`;
+}
