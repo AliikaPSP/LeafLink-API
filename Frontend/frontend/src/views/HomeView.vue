a3dc4292a385a29ff26ee21e31f044ac5454120a
riinojassoo <riin.ojassoo@hotmail.com>
2024-12-16 13:46:23 +0200

    Plantlists to homeview

--- Frontend/frontend/src/views/HomeView.vue
+++ Frontend/frontend/src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <script>
 import PlantsTable from '../components/PlantsTable.vue'
 import UsersTable from '../components/UsersTable.vue'
+import PlantListsTable from '@/components/PlantListsTable.vue';
 
 export default {
   components: { PlantsTable, UsersTable},
   data() { return {
     allPlants: [],
-    allUsers: []
+    allUsers: [],
+    allPlantlists: [],
   }},
   async Created() {
     this.allPlants = await (await fetch('http://localhost:8080/plants')).json()
     this.allUsers = await (await fetch('http://localhost:8080/users')).json()
+    this.allPlantlists = await (await fetch('http://localhost:8080/plantlists')).json()
   }
 }
 </script>
@@ -21,5 +24,6 @@ export default {
     
     "/>
     <UsersTable :items="allUsers"/>
+    <PlantListsTable :items="allPlantlists"/>
   </main>
 </template>
