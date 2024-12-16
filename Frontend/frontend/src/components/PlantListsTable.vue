b2e4fb4f5b14bf5fe49defc252e15a1a55665ef5
riinojassoo <riin.ojassoo@hotmail.com>
2024-12-16 13:45:02 +0200

    Plantlist table

---
+++ Frontend/frontend/src/components/PlantListsTable.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: "PlantListsTable",
+    props: {
+        items: Array
+    }
+}
+</script>
+
+<template>
+    <table class="table table-striped">
+        <thead class ="table-dark">
+            <tr>
+            <th>UserID</th>
+            <th>PlantID</th>  
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in items" :key="item.PlantListID">
+                <td>{{ item.UserID }}</td>
+                <td>{{ item.PlantID }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
