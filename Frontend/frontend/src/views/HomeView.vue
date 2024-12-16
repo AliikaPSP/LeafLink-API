bd487b41af23d52702788c359acc240987c39835
riinojassoo <riin.ojassoo@hotmail.com>
2024-12-16 13:50:04 +0200

    Index commented out and bug fix in homeview

--- Frontend/frontend/src/views/HomeView.vue
+++ Frontend/frontend/src/views/HomeView.vue
@@ -4,26 +4,26 @@ import UsersTable from '../components/UsersTable.vue'
 import PlantListsTable from '@/components/PlantListsTable.vue';
 
 export default {
-  components: { PlantsTable, UsersTable},
-  data() { return {
-    allPlants: [],
-    allUsers: [],
-    allPlantlists: [],
-  }},
-  async Created() {
-    this.allPlants = await (await fetch('http://localhost:8080/plants')).json()
-    this.allUsers = await (await fetch('http://localhost:8080/users')).json()
-    this.allPlantlists = await (await fetch('http://localhost:8080/plantlists')).json()
+  components: { PlantsTable, UsersTable, PlantListsTable },
+  data() {
+    return {
+      allPlants: [],
+      allUsers: [],
+      allPlantlists: [],
+    };
+  },
+  async created() {
+    this.allPlants = await (await fetch('http://localhost:8080/plants')).json();
+    this.allUsers = await (await fetch('http://localhost:8080/users')).json();
+    this.allPlantlists = await (await fetch('http://localhost:8080/plantlists')).json();
   }
 }
 </script>
 
 <template>
   <main>
-    <PlantsTable :items="allPlants
-    
-    "/>
-    <UsersTable :items="allUsers"/>
-    <PlantListsTable :items="allPlantlists"/>
+    <PlantsTable :items="allPlants" />
+    <UsersTable :items="allUsers" />
+    <PlantListsTable :items="allPlantlists" />
   </main>
 </template>
--- LeafLink-API/index.js
+++ LeafLink-API/index.js
@@ -63,110 +63,110 @@ const {sync} = require('./db');
 //     }
 // ]
 
-const plantlists = [
-    {
-        PlantListID : 1,
-        UserID: [1],
-        PlantID: [1]
-    },
-    {
-        PlantListID : 2,
-        UserID: [2],
-        PlantID: [3]
-    }
-]
+// const plantlists = [
+//     {
+//         PlantListID : 1,
+//         UserID: [1],
+//         PlantID: [1]
+//     },
+//     {
+//         PlantListID : 2,
+//         UserID: [2],
+//         PlantID: [3]
+//     }
+// ]
 
-app.use(cors());
-app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
-app.use(express.json()); //for parsing application/json
+// app.use(cors());
+// app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+// app.use(express.json()); //for parsing application/json
 
-app.get('/', (req, res) => {
-    res.send(`Server running. Documentation at <a href="http://${host}:${port}/docs">/docs</a>`);
-})
+// app.get('/', (req, res) => {
+//     res.send(`Server running. Documentation at <a href="http://${host}:${port}/docs">/docs</a>`);
+// })
 
-require("./routes/plantRoutes")(app);
+// require("./routes/plantRoutes")(app);
 
-//plantlists GET - Returns a list of all plantlists in the API to the requesting user.
-app.get('/plantlists', (req, res) => {res.send(plantlists)});
-//plantlists DETAILS : As an app developer, I want to get full DETAILS of my given plantlist to show in my app 
-app.get('/plantlists/:id', (req, res) => {
-    if (typeof plantlists[req.params.id-1]
-         === 'undefined') {
-        return res.status(404).send({error: "Plantlist not found"});  //404 Not Found status code   
-    }
-    if (req.params.id == null) {
-        return res.status(400).send({error: "Invalid plantlist ID provided"});  //400 Bad Request status code
-    }
-    res.send(plantlists[req.params.id-1])
-})
-//plantlists CREATE - As an app developer, I want to be able to ADD a new Plantlist in my app
-app.post('/plantlists', (req, res) => {
-    if (!req.body.PlantID || 
-        !req.body.UserID ) {
-        return res.send(400).send({error: "One or multiple parameters are missing"});
-    }
+// //plantlists GET - Returns a list of all plantlists in the API to the requesting user.
+// app.get('/plantlists', (req, res) => {res.send(plantlists)});
+// //plantlists DETAILS : As an app developer, I want to get full DETAILS of my given plantlist to show in my app 
+// app.get('/plantlists/:id', (req, res) => {
+//     if (typeof plantlists[req.params.id-1]
+//          === 'undefined') {
+//         return res.status(404).send({error: "Plantlist not found"});  //404 Not Found status code   
+//     }
+//     if (req.params.id == null) {
+//         return res.status(400).send({error: "Invalid plantlist ID provided"});  //400 Bad Request status code
+//     }
+//     res.send(plantlists[req.params.id-1])
+// })
+// //plantlists CREATE - As an app developer, I want to be able to ADD a new Plantlist in my app
+// app.post('/plantlists', (req, res) => {
+//     if (!req.body.PlantID || 
+//         !req.body.UserID ) {
+//         return res.send(400).send({error: "One or multiple parameters are missing"});
+//     }
     
-    let plantlist = {
-        PlantListID: plantlists.length + 1,
-        PlantID: req.body.PlantID,
-        UserID: req.body.UserID
-    }
-    plantlists.push(plantlist);
-    res.status(201).location('${getBaseURL(req)}/plantlists/${plantlists.length}').send(plantlist);
-})
+//     let plantlist = {
+//         PlantListID: plantlists.length + 1,
+//         PlantID: req.body.PlantID,
+//         UserID: req.body.UserID
+//     }
+//     plantlists.push(plantlist);
+//     res.status(201).location('${getBaseURL(req)}/plantlists/${plantlists.length}').send(plantlist);
+// })
 
-//plantlist Update - As an app developer, I want to be able to UPDATE Plantlists
-app.put('/plantlists/:id', (req, res) => {
-    const plantlist = getPlantlist(req, res);
+// //plantlist Update - As an app developer, I want to be able to UPDATE Plantlists
+// app.put('/plantlists/:id', (req, res) => {
+//     const plantlist = getPlantlist(req, res);
 
-    if(!plantlist) {
-        return res.status(404).send({error: "Plantlist not found"});  //404 Not Found status code   
-    }
-    if (!req.body.PlantID || 
-        !req.body.UserID ) {
-        return res.status(400).send({error: "One or multiple parameters are missing"});
-    }
-        plantlist.PlantListID = req.body.PlantListID,
-        plantlist.UserID = req.body.UserID,
-        plantlist.PlantID = req.body.PlantID
+//     if(!plantlist) {
+//         return res.status(404).send({error: "Plantlist not found"});  //404 Not Found status code   
+//     }
+//     if (!req.body.PlantID || 
+//         !req.body.UserID ) {
+//         return res.status(400).send({error: "One or multiple parameters are missing"});
+//     }
+//         plantlist.PlantListID = req.body.PlantListID,
+//         plantlist.UserID = req.body.UserID,
+//         plantlist.PlantID = req.body.PlantID
     
-    plantlists.splice((req.body.PlantListID-1), 1, plantlist);
-        res.status(201)
-        .location(`${getBaseURL(req)}/plantlists/${req.params.id}`)
-        .send(plantlist);
+//     plantlists.splice((req.body.PlantListID-1), 1, plantlist);
+//         res.status(201)
+//         .location(`${getBaseURL(req)}/plantlists/${req.params.id}`)
+//         .send(plantlist);
 
-})
+// })
 
-//plantlist DELETE - As an app developer, I want to be able to DELETE a plantlist in my app
-app.delete('/plantlists/:id', (req, res) => {
-    if(typeof plantlists[req.params.id-1] === 'undefined') {
-        return res.status(404).send({error: "Plantlist not found"});  //404 Not Found status code
-    }
-    plantlists.splice((req.params.id-1), 1);
+// //plantlist DELETE - As an app developer, I want to be able to DELETE a plantlist in my app
+// app.delete('/plantlists/:id', (req, res) => {
+//     if(typeof plantlists[req.params.id-1] === 'undefined') {
+//         return res.status(404).send({error: "Plantlist not found"});  //404 Not Found status code
+//     }
+//     plantlists.splice((req.params.id-1), 1);
 
-    res.status(204).send({error: "No content"}); //204 No Content status code
-})
-
-
-
-app.listen(port, async() => {
-    if (process.env.SYNC === 'true') {
-        await sync();
-    }
-    console.log(`Api on saadaval aadressil: http://${host}:${port}`);
-});    
+//     res.status(204).send({error: "No content"}); //204 No Content status code
+// })
 
 
-function getPlantlist(req, res) {
-    const idNumber = parseInt(req.params.id, 10);
-    if(isNaN(idNumber)) {
-        res.status(400).send({error: "Invalid plantlist ID provided"});
-        return null;
-    }
-    const plantlist = plantlists.find(p => p.PlantListID === idNumber);
-    if (!plantlist) {
-        res.status(404).send({error: "Plantlist not found"});
-        return null;
-    }
-    return plantlist;
-}
+
+// app.listen(port, async() => {
+//     if (process.env.SYNC === 'true') {
+//         await sync();
+//     }
+//     console.log(`Api on saadaval aadressil: http://${host}:${port}`);
+// });    
+
+
+// function getPlantlist(req, res) {
+//     const idNumber = parseInt(req.params.id, 10);
+//     if(isNaN(idNumber)) {
+//         res.status(400).send({error: "Invalid plantlist ID provided"});
+//         return null;
+//     }
+//     const plantlist = plantlists.find(p => p.PlantListID === idNumber);
+//     if (!plantlist) {
+//         res.status(404).send({error: "Plantlist not found"});
+//         return null;
+//     }
+//     return plantlist;
+// }
