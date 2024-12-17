4513419b525c5aa7363380daae443a0bd8990cd1
riinojassoo <riin.ojassoo@hotmail.com>
2024-12-17 10:33:41 +0200

    Homeview edited, only title

--- Frontend/frontend/src/views/HomeView.vue
+++ Frontend/frontend/src/views/HomeView.vue
@@ -1,29 +1,10 @@
-<script>
-import PlantsTable from '../components/PlantsTable.vue'
-import UsersTable from '../components/UsersTable.vue'
-import PlantListsTable from '../components/PlantListsTable.vue';
-
-export default {
-  components: { PlantsTable, UsersTable, PlantListsTable },
-  data() {
-    return {
-      allPlants: [],
-      allUsers: [],
-      allPlantlists: [],
-    };
-  },
-  async created() {
-    this.allPlants = await (await fetch('http://localhost:8080/plants')).json();
-    this.allUsers = await (await fetch('http://localhost:8080/users')).json();
-    this.allPlantlists = await (await fetch('http://localhost:8080/plantlists')).json();
-  }
-}
-</script>
-
 <template>
   <main>
-    <PlantsTable :items="allPlants" />
-    <UsersTable :items="allUsers" />
-    <PlantListsTable :items="allPlantlists" />
+    <h1>LeafLink Collaboration API</h1>
   </main>
 </template>
+
+<script setup>
+// No additional logic required
+</script>
+
