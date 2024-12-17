0da186fcc98fcad10b322e0fcc0cf3704a87128a
riinojassoo <riin.ojassoo@hotmail.com>
2024-12-17 10:07:36 +0200

    Plant lists view

---
+++ Frontend/frontend/src/views/PlantListsView.vue
@@ -0,0 +1,21 @@
+<script>
+import PlantListsTable from '../components/PlantListsTable.vue';
+
+export default {
+  components: { PlantListsTable },
+  data() {
+    return {
+      allPlantlists: []
+    };
+  },
+  async created() {
+    this.allPlantlists = await (await fetch('http://localhost:8080/plantlists')).json();
+  }
+}
+</script>
+
+<template>
+  <main>
+    <PlantListsTable :items="allPlantlists" />
+  </main>
+</template>
