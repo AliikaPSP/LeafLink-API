af16aa4c08b1bbfc30fb7d0c26b1fb7f7012ed0d
riinojassoo <riin.ojassoo@hotmail.com>
2024-12-17 10:04:30 +0200

    Plants view added

---
+++ Frontend/frontend/src/views/PlantsView.vue
@@ -0,0 +1,21 @@
+<script>
+import PlantsTable from '../components/PlantsTable.vue'
+
+export default {
+  components: { PlantsTable },
+  data() {
+    return {
+      allPlants: []
+    };
+  },
+  async created() {
+    this.allPlants = await (await fetch('http://localhost:8080/plants')).json();
+  }
+}
+</script>
+
+<template>
+  <main>
+    <PlantsTable :items="allPlants" />
+  </main>
+</template>
