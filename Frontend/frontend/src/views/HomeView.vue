8adc558a0381c66da9e28b2cda8c77652a7ab862
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2024-12-17 12:30:04 +0200

    As a developer i want the frontend to have a clean and organized design: Homeview/Header/Footer

--- Frontend/frontend/src/views/HomeView.vue
+++ Frontend/frontend/src/views/HomeView.vue
@@ -1,10 +1,23 @@
 <template>
   <main>
-    <h1>LeafLink Collaboration API</h1>
+    
   </main>
+
+  <v-card-title primary-title class="justify-center">
+  <div>
+    <h3 class="headline" style="color: #7D9883; text-align: center;">LeafLink Collaboration API</h3>
+    <img src="https://cdn.pixabay.com/photo/2023/06/15/15/57/cactus-8065902_960_720.png" alt="Cactus Image" style="width: 200px; height: 200px; display: block; margin: 0 auto;" />
+  </div>
+</v-card-title>
+  
 </template>
 
+<style>
+
+h1 {
+  color: #56806F; /* Change this to your preferred color */
+}
+</style>
 <script setup>
 // No additional logic required
 </script>
-
