cb4fba981be9068de13fa63fd75bec8e282aaf9f
riinojassoo <riin.ojassoo@hotmail.com>
2025-01-06 17:01:59 +0200

    homeview fixed

--- Frontend/frontend/src/views/HomeView.vue
+++ Frontend/frontend/src/views/HomeView.vue
@@ -1,14 +1,10 @@
 <template>
-  <main>
-    
-  </main>
 
-  <v-card-title primary-title class="justify-center">
-  <div>
-    <h3 class="headline" style="color: #7D9883; text-align: center;">LeafLink Collaboration API</h3>
-    <img src="https://cdn.pixabay.com/photo/2023/06/15/15/57/cactus-8065902_960_720.png" alt="Cactus Image" style="width: 200px; height: 200px; display: block; margin: 0 auto;" />
-  </div>
-</v-card-title>
+    <div class="row d-flex justify-content-center">
+      <h3 class="headline" style="color: #7D9883; text-align: center;">LeafLink Collaboration API</h3>
+      <img src="https://cdn.pixabay.com/photo/2023/06/15/15/57/cactus-8065902_960_720.png" alt="Cactus Image" style="width: 200px; height: 200px; display: block; margin: 0 auto;" />
+    </div>
+
   
 </template>
 
