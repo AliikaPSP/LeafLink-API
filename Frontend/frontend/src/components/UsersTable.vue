9995dd36efa3f6a451604de13be8bcdd1f1ebe83
Aliika Puusepp <153951390+AliikaPSP@users.noreply.github.com>
2025-01-07 09:43:15 +0200

    As a user i want the frontend to have a clean and organized design: 5

--- Frontend/frontend/src/components/UsersTable.vue
+++ Frontend/frontend/src/components/UsersTable.vue
@@ -8,8 +8,8 @@ export default {
 </script>
 
 <template>
-    <table class="table table-striped">
-        <thead class ="table-dark">
+    <table class="custom-table">
+        <thead>
             <tr>
                 <th>UserID</th>
                 <th>UserName</th> 
@@ -25,4 +25,36 @@ export default {
 </template>
 
 <style scoped>
+.custom-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin: 0px 0;
+  font-size: 1rem;
+  text-align: left;
+  background-color: #e6e4d7; /* Body background color */
+}
+
+.custom-table thead {
+  background-color: #7D9883; /* Header background color */
+  color: white;
+  font-weight: bold;
+}
+
+.custom-table th, .custom-table td {
+  border: 1px solid #ddd;
+  padding: 12px 15px;
+}
+
+/* Change body text color */
+.custom-table tbody tr td {
+  color: #618068;
+}
+
+.custom-table tbody tr:nth-child(even) {
+  background-color: #f4f4f4; /* Stripe effect */
+}
+
+.custom-table tbody tr:hover {
+  background-color: #e0e0e0; /* Hover effect */
+}
 </style>
